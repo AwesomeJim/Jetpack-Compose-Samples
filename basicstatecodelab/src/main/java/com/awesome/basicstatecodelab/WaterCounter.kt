0df6c57077b1b02bb59b79e683fe543fc6777b1d
awesomejim <jimnahgua@gmail.com>
2022-09-04 12:56:10 +0300

    Update: - 8. Restore state in Compose While remember helps you retain state across recompositions, it's not retained across configuration changes. For this, you must use rememberSaveable instead of remember

--- basicstatecodelab/src/main/java/com/awesome/basicstatecodelab/WaterCounter.kt
+++ basicstatecodelab/src/main/java/com/awesome/basicstatecodelab/WaterCounter.kt
@@ -1,11 +1,14 @@
 package com.awesome.basicstatecodelab
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -14,19 +17,12 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     // Changes to count are now tracked by Compose
-    var count by remember { mutableStateOf(0) }
+    var count by rememberSaveable { mutableStateOf(0) }
     Column(
         modifier = modifier.padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         if (count > 0) {
-            var showTask by remember { mutableStateOf(true) }
-            if (showTask) {
-                WellnessTaskItem(
-                    onClose = { showTask = false },
-                    taskName = "Have you taken your 15 minute walk today?"
-                )
-            }
             // This text is present if the button has been clicked
             // at least once; absent otherwise
             Text(
@@ -34,22 +30,14 @@ fun WaterCounter(modifier: Modifier = Modifier) {
                 modifier = modifier.padding(16.dp)
             )
         }
-        Row(Modifier.padding(top = 8.dp)) {
-            Button(
-                onClick = { count++ },
-                enabled = count < 10
-            ) {
-                Text("Add one")
-
-            }
-            Button(
-                onClick = { count = 0 },
-                modifier = Modifier.padding(start = 8.dp)
-            ) {
-                Text("Clear water count")
-            }
-
+        Button(
+            onClick = { count++ },
+            enabled = count < 10,
+            modifier = Modifier.padding(top = 8.dp)
+        ) {
+            Text("Add one")
         }
+
     }
 }
 
