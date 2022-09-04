adb5f61e52b6263e481cf615490d7dfa32c5c2e6
awesomejim <jimnahgua@gmail.com>
2022-09-04 13:34:06 +0300

    Update: - 10. Work with lists

--- basicstatecodelab/src/main/java/com/awesome/basicstatecodelab/WellnessTaskItem.kt
+++ basicstatecodelab/src/main/java/com/awesome/basicstatecodelab/WellnessTaskItem.kt
@@ -2,12 +2,17 @@ package com.awesome.basicstatecodelab
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Checkbox
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -17,6 +22,8 @@ import com.awesome.basicstatecodelab.ui.theme.MyComposeApplicationTheme
 @Composable
 fun WellnessTaskItem(
     taskName: String,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
     onClose: () -> Unit,
     modifier: Modifier = Modifier
 ) {
@@ -30,10 +37,26 @@ fun WellnessTaskItem(
                 .padding(start = 16.dp),
             text = taskName
         )
+        Checkbox(
+            checked = checked,
+            onCheckedChange = onCheckedChange
+        )
         IconButton(onClick = onClose) {
             Icon(Icons.Filled.Close, contentDescription = "Close")
         }
     }
+}
+
+@Composable
+fun WellnessTaskItem(taskName: String, modifier: Modifier = Modifier) {
+    var checkedState by rememberSaveable { mutableStateOf(false) }
+    WellnessTaskItem(
+        taskName = taskName,
+        checked = checkedState,
+        onCheckedChange = { newValue -> checkedState = newValue },
+        onClose = {}, // we will implement this later!
+        modifier = modifier,
+    )
 }
 
 
@@ -42,7 +65,7 @@ fun WellnessTaskItem(
 fun WellnessTaskItemPreview() {
     MyComposeApplicationTheme {
         WellnessTaskItem(
-            taskName = "Drink water", onClose = {},
+            taskName = "Drink water",
             modifier = Modifier.padding(8.dp)
         )
     }
