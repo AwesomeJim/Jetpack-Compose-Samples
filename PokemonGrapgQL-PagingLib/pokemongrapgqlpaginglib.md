8f58418806fcbe2cba7b32803b5934fc761281bd
AwesomeJim <jimnahgua@gmail.com>
2023-07-05 12:28:14 +0300

    Update: Added pokemon grapg ql paging lib : Module - Added Navigation Test

---
+++ PokemonGrapgQL-PagingLib/pokemongrapgqlpaginglib.md
@@ -0,0 +1,4 @@
+Paging in Android Jetpack Compose
+-------------
+
+[Paging in Android Jetpack Compose: From Caching Data with Room to Displaying in LazyColumn](https://www.droidcon.com/2023/07/03/paging-in-android-jetpack-compose-from-caching-data-with-room-to-displaying-in-lazycolumn/)
--- affirmations/src/main/java/com/example/affirmations/MainActivity.kt
+++ affirmations/src/main/java/com/example/affirmations/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.affirmations
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -179,14 +180,20 @@ fun TopicsList(topicsList: List<Topic>, modifier: Modifier = Modifier) {
 
 
 @Preview(name = "TopicItem")
+@Preview(name = "TopicItem Dark", uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 private fun TopicCardPreview() {
-    TopicCard(Topic(R.string.photography, 23, R.drawable.photography))
+    AffirmationsTheme {
+        TopicCard(Topic(R.string.photography, 23, R.drawable.photography))
+    }
 }
 
 
 @Preview(name = "TopicItem")
+@Preview(name = "TopicItem Dark", uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 private fun TopicsListPreview() {
-    TopicsList(topicsList = Datasource().topics)
+    AffirmationsTheme {
+        TopicsList(topicsList = Datasource().topics)
+    }
 }
