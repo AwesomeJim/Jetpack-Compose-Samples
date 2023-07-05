0549b1e07b1de1ae6f34122ac874aa1bbbce4ad9
AwesomeJim <jimnahgua@gmail.com>
2023-07-05 14:59:53 +0300

    Update: Added Apollo Kotlin SDK for GraphQ :Module - get Space X launches List and View Details

--- ApolloGraphQL/src/main/java/com/example/rocketreserver/LaunchDetails.kt
+++ ApolloGraphQL/src/main/java/com/example/rocketreserver/LaunchDetails.kt
@@ -1,6 +1,5 @@
 package com.example.rocketreserver
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -15,46 +14,90 @@ import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.apollographql.apollo3.api.Error
+import com.apollographql.apollo3.exception.ApolloException
+import com.example.rocketreserver.remote.apolloClient
+
+private sealed interface LaunchDetailsState {
+    object Loading : LaunchDetailsState
+    data class ProtocolError(val exception: ApolloException) : LaunchDetailsState
+    data class ApplicationError(val errors: List<Error>) : LaunchDetailsState
+    data class Success(val data: LaunchDetailsQuery.Data) : LaunchDetailsState
+
+}
 
 @Composable
 fun LaunchDetails(launchId: String) {
+    var state by remember { mutableStateOf<LaunchDetailsState>(LaunchDetailsState.Loading) }
+    LaunchedEffect(Unit) {
+        state = try {
+            val response = apolloClient.query(LaunchDetailsQuery(launchId)).execute()
+            if (response.hasErrors()) {
+                LaunchDetailsState.ApplicationError(response.errors!!)
+            } else {
+                LaunchDetailsState.Success(response.data!!)
+            }
+        } catch (e: ApolloException) {
+            LaunchDetailsState.ProtocolError(e)
+
+        }
+
+    }
+    // Use the state
+    when (val s = state) {
+        LaunchDetailsState.Loading -> Loading()
+        is LaunchDetailsState.ProtocolError -> ErrorMessage("Oh no... A protocol error happened: ${s.exception.message}")
+        is LaunchDetailsState.Success -> LaunchDetails(s.data)
+        is LaunchDetailsState.ApplicationError -> ErrorMessage("Oh no... A protocol error happened: ${s.errors}")
+    }
+
+}
+
+@Composable
+private fun LaunchDetails(response: LaunchDetailsQuery.Data) {
     Column(
         modifier = Modifier.padding(16.dp)
     ) {
         Row(verticalAlignment = Alignment.CenterVertically) {
             // Mission patch
-            Image(
+            AsyncImage(
                 modifier = Modifier.size(160.dp, 160.dp),
-                painter = painterResource(R.drawable.ic_placeholder),
+                model = response.launch?.mission?.missionPatch,
+                placeholder = painterResource(R.drawable.ic_placeholder),
+                error = painterResource(R.drawable.ic_placeholder),
                 contentDescription = "Mission patch"
             )
-
             Spacer(modifier = Modifier.size(16.dp))
-
             Column {
                 // Mission name
                 Text(
                     style = MaterialTheme.typography.headlineMedium,
-                    text = "Launch $launchId"
+                    text = response.launch?.mission?.name ?: ""
                 )
 
                 // Rocket name
                 Text(
                     modifier = Modifier.padding(top = 8.dp),
                     style = MaterialTheme.typography.headlineSmall,
-                    text = "Rocket name",
+                    text = response.launch?.rocket?.name?.let { "🚀 $it" } ?: "",
                 )
 
                 // Site
                 Text(
                     modifier = Modifier.padding(top = 8.dp),
                     style = MaterialTheme.typography.titleMedium,
-                    text = "Site..."
+                    text = response.launch?.site ?: "",
                 )
             }
         }
