2c4a08beac11e0cc490029cbdec56c452954208e
AwesomeJim <jimnahgua@gmail.com>
2023-07-05 21:05:47 +0300

    Update: Added Apollo Kotlin SDK for GraphQ :Module - Subscription

--- ApolloGraphQL/src/main/java/com/example/rocketreserver/remote/Apollo.kt
+++ ApolloGraphQL/src/main/java/com/example/rocketreserver/remote/Apollo.kt
@@ -1,8 +1,10 @@
 package com.example.rocketreserver.remote
 
+import android.util.Log
 import com.apollographql.apollo3.ApolloClient
 import com.apollographql.apollo3.network.okHttpClient
 import com.example.rocketreserver.TokenRepository
+import kotlinx.coroutines.delay
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Response
@@ -10,6 +12,13 @@ import okhttp3.Response
 
 val apolloClient = ApolloClient.Builder()
     .serverUrl("https://apollo-fullstack-tutorial.herokuapp.com/graphql")
+    .webSocketServerUrl("wss://apollo-fullstack-tutorial.herokuapp.com/graphql")
+    .webSocketReopenWhen { throwable, attempt ->
+        Log.d("Apollo", "WebSocket got disconnected, reopening after a delay", throwable)
+        delay(attempt * 1000)
+        true
+
+    }
     .okHttpClient(
         OkHttpClient.Builder()
             .addInterceptor(AuthorizationInterceptor())
