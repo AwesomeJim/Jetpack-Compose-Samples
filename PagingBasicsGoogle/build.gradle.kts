35d78500a2cc036e43e881f94769e93f3fcebd36
AwesomeJim <jimnahgua@gmail.com>
2023-07-06 12:46:14 +0300

    Update: PaggingBasics CodeLab :Module - initial commit

---
+++ PagingBasicsGoogle/build.gradle.kts
@@ -0,0 +1,65 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.com.android.application)
+    alias(libs.plugins.org.jetbrains.kotlin.android)
+    id(libs.plugins.com.google.devtools.ksp.get().pluginId)
+    id(libs.plugins.kapt.get().pluginId)
+}
+android {
+    compileSdk = libs.versions.compile.sdk.get().toInt()
+
+    defaultConfig {
+        applicationId = "com.google.samples.apps.nowinandroid"
+        minSdk = libs.versions.min.sdk.get().toInt()
+        targetSdk = libs.versions.target.sdk.get().toInt()
+        versionCode = 1
+        versionName = "0.0.1"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    kotlinOptions {
+        jvmTarget = libs.versions.jvm.target.get()
+        freeCompilerArgs += listOf("-Xopt-in=kotlin.RequiresOptIn")
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+        // coreLibraryDesugaringEnabled= true
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+    namespace = "com.example.android.codelabs.paging"
+}
+
+dependencies {
+
+
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.recyclerview)
+    implementation(libs.androidx.constraintlayout)
+
+    implementation(libs.com.google.android.material)
+
+    // architecture components
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.paging.common.ktx)
+    implementation(libs.androidx.paging.runtime.ktx)
+}
---
+++ PagingPokemonGrapgQL/src/main/java/com/awesomejim/pokemongrapgqlpaginglib/MainActivity.kt
@@ -0,0 +1,90 @@
+package com.awesomejim.pokemongrapgqlpaginglib
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.navigation.NamedNavArgument
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavDeepLink
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.get
+import androidx.navigation.navArgument
+import com.awesomejim.pokemongrapgqlpaginglib.ui.custom.TopBar
+import com.awesomejim.pokemongrapgqlpaginglib.ui.pokeman.detail.PokemonDetailScreen
+import com.awesomejim.pokemongrapgqlpaginglib.ui.pokeman.list.PokemonListScreen
+import com.awesomejim.pokemongrapgqlpaginglib.ui.theme.PokemonPagerTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PokemonPagerTheme {
+                val navController = rememberNavController()
+                val snackbarHostState = remember { SnackbarHostState() }
+
+                Scaffold(
+                    snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
+                    topBar = { TopBar(navController = navController) },
+                ) { padding ->
+                    NavHost(
+                        modifier = Modifier.padding(padding),
+                        navController = navController,
+                        startDestination = "pokemon-list",
+                    ) {
+                        composable(
+                            "pokemon-list",
+                            label = "Gotta Catch 'Em All!",
+                        ) {
+                            PokemonListScreen(
+                                navigateToDetail = { id ->
+                                    navController.navigate("pokemon/$id")
+                                },
+                                snackbarHostState = snackbarHostState,
+                            )
+                        }
+                        composable(
+                            "pokemon/{id}",
+                            label = "Pokemon",
+                            arguments = listOf(navArgument("id") { type = NavType.IntType }),
+                        ) {
+                            PokemonDetailScreen(snackbarHostState = snackbarHostState)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+fun NavGraphBuilder.composable(
+    route: String,
+    label: String,
+    arguments: List<NamedNavArgument> = emptyList(),
+    deepLinks: List<NavDeepLink> = emptyList(),
+    content: @Composable (NavBackStackEntry) -> Unit
+) {
+    addDestination(ComposeNavigator.Destination(provider[ComposeNavigator::class], content).apply {
+        this.route = route
+        this.label = label // SET LABEL
+        arguments.forEach { (argumentName, argument) ->
+            addArgument(argumentName, argument)
+        }
+        deepLinks.forEach { deepLink ->
+            addDeepLink(deepLink)
+        }
+    })
+}
